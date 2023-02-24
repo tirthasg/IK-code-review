eaf16c136cbe1cde92563b17b02445992c59f914
Tirtha <tirthasg@gmail.com>
2023-02-24 19:36:21 +0530

    comments added to code for documentation

--- 01_top_k_frequent.cpp
+++ 01_top_k_frequent.cpp
@@ -1,38 +1,56 @@
 void Helper(vector<vector<int>> &nums, int start, int end, int index)
 {
+    // Base case or work done by leaf level worker
     if (start == end)
         return;
-        
+    
+    // Work done by arbitrary worker in the hierarchy
+    // Selecting a random index in our sub-array 
     int randIndex = start + rand() % (end - start + 1);
+
+    // Swapping the element at random index with the element at starting index
     swap(nums[start], nums[randIndex]);
     
+    // Storing the pivot into a temp variable of type vector<int>
     auto pivot = nums[start];
+
+    // Lomuto's partition
     int smaller = start;
     for (int bigger = smaller + 1; bigger <= end; ++bigger)
+        // Comparison is done based on frequency
         if (nums[bigger][1] <= pivot[1])
             swap(nums[++smaller], nums[bigger]);
-        
+    
+    // Swapping with the location of pivot
     swap(nums[start], nums[smaller]);
     
+    // Lucky case where the chosen pivot is the k'th most frequent element
     if (smaller == index)
         return;
-        
+    
+    // Recursively perfom Quick Select on right sub-array
     if (smaller < index) {
         Helper(nums, smaller + 1, end, index);
         return;
     }
     
+    // Recursively perform Quick Select on left sub-array
     Helper(nums, start, smaller - 1, index);
 }
 
+// Quick Select implementation returning the top k frequent elements as a vector
 vector<int> QuickSelect(vector<vector<int>> &nums, int k)
 {
     int N = nums.size();
     vector<int> result;
     
+    // Seeding random number generator
     srand(time(0));
+
+    // Calling Helper function to implement the Quick Select startegy recursively
     Helper(nums, 0, N - 1, N - k);
     
+    // Saving the top k frequent elements in resultant vector
     for (int i = N - k; i < N; ++i)
         result.push_back(nums[i][0]);
         
@@ -43,7 +61,8 @@ vector<int> QuickSelect(vector<vector<int>> &nums, int k)
 vector<int> find_top_k_frequent_elements(vector<int> &arr, int k) {
     // Write your code here.
     int N = arr.size();
-    
+
+    // Storing the frequency table as a hashmap    
     unordered_map<int, int> hmap;
     for (int i = 0; i < N; ++i)
         if (hmap.count(arr[i]))
@@ -51,9 +70,12 @@ vector<int> find_top_k_frequent_elements(vector<int> &arr, int k) {
         else
             hmap[arr[i]] = 1;
     
+    // Creating a vector of vector from the hashmap
+    // Each vector stores element & it's corresponding frequency
     vector<vector<int>> nums;
     for (auto kvp: hmap)
         nums.push_back({kvp.first, kvp.second});
-        
+
+    // Calling Quick Select to return top k frequent elements as a vector        
     return QuickSelect(nums, k);
 }
--- 02_nearest_neighbours.cpp
+++ 02_nearest_neighbours.cpp
@@ -1,3 +1,6 @@
+// Function to calculate distance of any point from the reference point
+// Actually the square of distance 
+// Saving computation time by not performing the square root operation
 long long Distance(vector<int> &point, int p_x, int p_y)
 {
     long long xDiff = point[0] - p_x;
@@ -8,30 +11,44 @@ long long Distance(vector<int> &point, int p_x, int p_y)
 
 void Helper(vector<vector<int>> &n_points, int start, int end, int index, int p_x, int p_y)
 {
+    // Base case or work done by leaf level worker
     if (start == end)
         return;
-        
+    
+    // Work done by arbitrary worker in the hierarchy
+    // Selecting a random index in our sub-array 
     int randIndex = start + rand() % (end - start + 1);
+
+    // Swapping the element at random index with the element at starting index
     swap(n_points[start], n_points[randIndex]);
     
+    // Storing the pivot into a temp variable of type vector<int> 
     auto pivot = n_points[start];
+
+    // Calculating the distance of pivot from the reference point
     long long pivotDist = Distance(pivot, p_x, p_y);
     
+    // Lomuto's partition
     int smaller = start;
     for (int bigger = start + 1; bigger <= end; ++bigger)
+        // Comparison is done based on distance from reference point
         if (Distance(n_points[bigger], p_x, p_y) <= pivotDist)
             swap(n_points[++smaller], n_points[bigger]);
     
+    // Swapping with the location of pivot
     swap(n_points[start], n_points[smaller]);
     
+    // Lucky case where the chosen pivot is the k'th most frequent element
     if (smaller == index)
         return;
-        
+    
+    // Recursively perfom Quick Select on right sub-array
     if (smaller < index) {
         Helper(n_points, smaller + 1, end, index, p_x, p_y);
         return;
     }
     
+    // Recursively perform Quick Select on left sub-array
     Helper(n_points, start, smaller - 1, index, p_x, p_y);
 }
 
@@ -39,10 +56,14 @@ vector<vector<int>> nearest_neighbours(int p_x, int p_y, int k, vector<vector<in
     // Write your code here.
     int N = n_points.size();
     vector<vector<int>> result(k);
-        
+    
+    // Seeding the random number generator
     srand(time(0));
+
+    // Calling Helper function to implement the Quick Select startegy recursively
     Helper(n_points, 0, N - 1, k - 1, p_x, p_y);
     
+    // Saving the k nearest points in resultant vector
     for (int i = 0; i < k; ++i)
         result[i] = n_points[i];
     
